7c08d5043f3c9fa85ac38298b5f966057a6ef650
Abdul-Hafeez-Sheikh <sheikhabdulhafeez31@gmail.com>
2024-08-29 13:39:05 +0500

    Add files via upload

---
+++ first js assingment/chapter5/index5.js
@@ -0,0 +1,49 @@
+//Question 1 //
+var number1 = 6;
+ var number2 = 9;
+
+            
+            var plus = number1 + number2;
+
+            
+            var result = "Sum of " + number1 + " and " + number2 + " is " + plus;
+
+            
+            document.write(result);
+            //Question 1 //
+            //.....//
+
+            //Question 2//
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var minus = number1 - number2;
+
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var subs = number1 * number2;
+
+            var number1 = 6;
+ var number2 = 9;
+
+            
+            var divi = number1 / number2;
+
+            var b = "minus of " + number1 + " and " - number2 + " is " + minus;
+
+            
+            document.write(b);
+
+            var c = "subs of " + number1 + " and " * number2 + " is " + subs;
+
+            
+            document.write(c);
+
+            var d = "divi of " + number1 + " and " / number2 + " is " + divi;
+
+            
+            document.write(d);
+
